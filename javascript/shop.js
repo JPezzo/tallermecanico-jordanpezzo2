623b02ddcd0c1b9b8cb1cb5011cede8b85147832
Jordán Pezzo <jordanpezzo@hotmail.com>
2022-04-02 12:49:31 -0300

    Cambié de lugar el script de JS porque funcionaba mal. Probablemente se pisaba con Bootstrap o AOS. También guardo mis avances del código. Hay un error de bucle infinito en el switch de ingreso

---
+++ javascript/shop.js
@@ -0,0 +1,57 @@
+
+// Variable de ingreso a la tienda y lista de productos
+
+let ingreso = prompt('¿Desea ingresar a la tienda online y ver la lista de productos? Por favor ingrese SI o NO').toUpperCase()
+
+// Función para ingresar producto y cantidad luego de haber visto la lista de productos
+
+function comprarProductos() {
+    let productoDeseado = prompt('Ingrese el producto deseado')
+    let cantidadDeseada = parseInt(prompt('Ingrese la cantidad deseada del producto'))
+
+    alert('Usted ha seleccionado ' + cantidadDeseada + ' items del producto: ' + productoDeseado + '.')
+
+    let masProductos = prompt('¿Desea seleccionar más productos? Por favor ingrese SI o NO').toUpperCase()
+
+    while(masProductos != 'NO') {
+        switch(masProductos) {
+            case 'SI':
+                comprarProductos();
+                break;
+            
+            case '':
+                alert('El campo no puede quedar vacío, debe ingresar SI o NO. Intente nuevamente.');
+                masProductos = prompt('¿Desea seleccionar más productos? Por favor ingrese SI o NO').toUpperCase();
+                break;
+    
+            default:
+                alert('El dato ingresado es incorrecto, debe ingresar SI o NO. Intente nuevamente.');
+                masProductos = prompt('¿Desea seleccionar más productos? Por favor ingrese SI o NO').toUpperCase();
+                break;
+        }
+    }
+    alert('¡Gracias por su compra!')
+}
+
+// Condicional para ingreso a la tienda y lista de productos
+
+while(ingreso != 'NO') {
+    switch(ingreso) {
+        case 'SI':
+            alert('Esta es nuestra lista de productos: A) Llave cruz - B) Refrigerante - C) Destornillador');
+            comprarProductos()
+            break;
+        
+        case '':
+            alert('El campo no puede quedar vacío, debe ingresar SI o NO. Intente nuevamente.');
+            ingreso = prompt('¿Desea ingresar a la tienda online y ver la lista de productos? Por favor ingrese SI o NO').toUpperCase();
+            break;
+
+        default:
+            alert('El dato ingresado es incorrecto, debe ingresar SI o NO. Intente nuevamente.');
+            ingreso = prompt('¿Desea ingresar a la tienda online y ver la lista de productos? Por favor ingrese SI o NO').toUpperCase();
+            break;
+    }
+}
+alert('¡Continúe navegando por nuestra página!')
+
