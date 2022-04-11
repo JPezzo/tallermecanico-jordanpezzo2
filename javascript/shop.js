8bec8b4050f3d31260d8bab62ff1ea01d92b0bfa
Jordán Pezzo <jordanpezzo@hotmail.com>
2022-04-11 17:39:37 -0300

    Correcciones Christhian

--- javascript/shop.js
+++ javascript/shop.js
@@ -1,10 +1,9 @@
-    
-// Listado de productos
+     
 class Producto {
-    constructor(name, price) {
+    constructor(id, name, price) {
+        this.id = id
         this.name = name.toUpperCase()
         this.price = parseFloat(price)
-        this.sold = false
     }
 
     addIVA() {
@@ -12,20 +11,27 @@ class Producto {
     }
 }
 
-// Carrito de compras
-const CARRITO = []
+const productos = [
+    {id: 1, name: 'Llave cruz', price: 1500},
+    {id: 2, name: 'Refrigerante', price: 850},
+    {id: 3, name: 'Destornillador', price: 500},
+    {id: 4, name: 'Bidón de aceite', price: 2500},
+]
+
+let productosObjeto = []
 
 // Función para ingreso a la tienda y lista de productos
 
 function ingreso1() {
     let ingreso = prompt('¿Desea ingresar a la tienda online y ver la lista de productos? Por favor ingrese SI o NO').toUpperCase();
 
+    whileDeIngreso:
     while(ingreso != 'NO') {
         switch(ingreso) {
             case 'SI':
-                alert('Este es nuestro listado de productos: 1) Llave cruz - $1.500.-; 2) Refrigerante - $850.-; 3) Destornillador - $500; 4) Bidón de aceite Elaion - $2.500.-')
+                alert('Este es nuestro listado de productos: id 1) Llave cruz - $1.500.-; id 2) Refrigerante - $850.-; id 3) Destornillador - $500; id 4) Bidón de aceite Elaion - $2.500.-')
                 comprarProductos();
-                break;
+                break whileDeIngreso;
             
             case '':
                 alert('El campo no puede quedar vacío, debe ingresar SI o NO. Intente nuevamente.');
@@ -42,16 +48,41 @@ function ingreso1() {
 }
 ingreso1();
 
+
 // Función para ingresar producto y cantidad luego de haber visto la lista de productos
-
 function comprarProductos() {
-    let productoDeseado = prompt('Ingrese el producto deseado').toUpperCase();
-    let cantidadDeseada = parseInt(prompt('Ingrese la cantidad deseada del producto'));
 
-    // Condicional que constata que los prompt no queden vacíos
-    if((productoDeseado == '') || (isNaN(cantidadDeseada))) {
-        alert('Debe ingresar un producto y un número válidos');
+    let productoDeseado = parseInt(prompt('Ingrese el producto deseado'));
+
+    if(productoDeseado === 1) {
+        buscar = productos.find((product) => product.id === 1)
+        console.log(buscar)
+        productos.forEach((prod) => {
+            const newProduct = new Producto(prod.id, prod.name, prod.price);
+            productosObjeto.push(newProduct);
+        })
+    } else if (productoDeseado === 2) {
+        buscar = productos.find((product) => product.id === 2)
+        console.log(buscar)
+        productos.forEach((prod) => {
+            const newProduct = new Producto(prod.id, prod.name, prod.price);
+            productosObjeto.push(newProduct);
+        })
+    } else if (productoDeseado === 3) {
+        buscar = productos.find((product) => product.id === 3)
+        console.log(buscar)
+    } else if (productoDeseado === 4) {
+        buscar = productos.find((product) => product.id === 4)
+        console.log(buscar)
+    } else {
+        alert('Debe ingresar un ID de producto válido');
         comprarProductos();
+    }
+
+    let cantidadDeseada = parseInt(prompt('Ingrese la cantidad deseada del producto'));
+    if(isNaN(cantidadDeseada)) {
+        alert('Debe ingresar una cantidad válida');
+        cantidadDeseada;
     } else {
         alert(`Usted ha seleccionado ${cantidadDeseada} items del producto: ${productoDeseado}.`);
     }
@@ -59,15 +90,17 @@ function comprarProductos() {
     masProductos1();
 }
 
+
 // Función que pregunta al usuario si desea seleccionar más productos
 function masProductos1() {
     let masProductos2 = prompt('¿Desea seleccionar más productos? Por favor ingrese SI o NO').toUpperCase();
 
+    whileMasProductos:
     while(masProductos2 != 'NO') {
         switch(masProductos2) {
             case 'SI':
                 comprarProductos();
-                break;
+                break whileMasProductos;
                 
             case '':
                 alert('El campo no puede quedar vacío, debe ingresar SI o NO. Intente nuevamente.');
@@ -80,6 +113,7 @@ function masProductos1() {
                 break;
         }
     }
+    console.log(productosObjeto)
     alert('¡Gracias por su compra!');
 }
 
